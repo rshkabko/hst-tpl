a14d0bc28965d792da81f0f2d7837850038958f7
Roman Shkabko <r.shkabko@flamix.email>
2025-10-15 19:46:19 +0300

    Update install

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ZIP_URL="https://github.com/rshkabko/hst-tpl/archive/refs/heads/main.zip"
+TMPDIR="$(mktemp -d)"
+
+echo "Downloading archive..."
+curl -fsSL -L "$ZIP_URL" -o "$TMPDIR/src.zip"
+
+echo "Extracting to tmp..."
+unzip -oq "$TMPDIR/src.zip" -d "$TMPDIR"
+ROOT="$(find "$TMPDIR" -mindepth 1 -maxdepth 1 -type d | head -n1)"
+
+echo "Cleaning up unnecessary files..."
+find "$ROOT" -type f \( \
+  -name ".gitignore" -o \
+  -name "install.sh" -o \
+  -name "README.md" -o \
+  -name ".DS_Store" \
+\) -print -delete
+find "$ROOT" -type d \( -name ".github" -o -name ".git" \) -prune -print -exec rm -rf {} +
+
+echo "Copying to root (soft overwrite)..."
+rsync -a "$ROOT/" /
+
+echo "Cleaning temporary files..."
+rm -rf "$TMPDIR"
+
+echo "Done."
